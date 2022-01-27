1ac7156e4e43e2d17c54bad33e7cd0ba4acb3e23
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2022-01-26 19:58:56 -0500

    Adding iOS Project

---
+++ shelf/shelf/shelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  shelfApp.swift
+//  shelf
+//
+//  Created by Quin’darius Lyles-Woods on January/26/2022.
+//
+
+import SwiftUI
+
+@main
+struct shelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
