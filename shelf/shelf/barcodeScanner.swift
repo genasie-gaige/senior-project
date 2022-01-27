d67357dd93c663db83904d3d7f6d5a0ce3ec995c
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2022-01-26 21:39:17 -0500

    Adding introductory camera code

---
+++ shelf/shelf/barcodeScanner.swift
@@ -0,0 +1,41 @@
+//
+//  barcodeScanner.swift
+//  shelf
+//
+//  Created by Quin’darius Lyles-Woods on January/26/2022.
+//
+
+import SwiftUI
+
+struct barcodeScanner: View {
+    var image: CGImage?
+    private let label = Text("Camera Feed")
+    var body: some View {
+        if let image = image {
+            GeometryReader { geometery in
+                //
+                Image(image,
+                      scale: 1.0,
+                      orientation: .upMirrored,
+                      label: label
+                )
+                    .resizable()
+                    .scaledToFill()
+                    .frame(
+                        width: geometery.size.width,
+                        height: geometery.size.height,
+                        alignment: .center
+                    )
+                    .clipped()
+            }
+        }
+        Text("I will be a barcode scanner one day")
+            .navigationTitle("Scanner")
+    }
+}
+
+struct barcodeScanner_Previews: PreviewProvider {
+    static var previews: some View {
+        barcodeScanner()
+    }
+}
