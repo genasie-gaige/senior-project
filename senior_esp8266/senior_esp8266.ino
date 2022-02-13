af43cf6c1604ce14ed4764a3e8a476292b54115b
genasie-gaige <genasie.gaige@gmail.com>
2022-02-12 19:54:00 -0500

    updated arduino code and deleted flutter

--- senior_esp8266/senior_esp8266.ino
+++ senior_esp8266/senior_esp8266.ino
@@ -1,52 +1,42 @@
 #include <ESP8266WiFi.h>
 #include <SoftwareSerial.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
 
-const char *ssid = "TamGenDanny";
-const char *password = "TamGenDanny811";
 WiFiServer server(80);
-SoftwareSerial NodeMCU(D2,D3);
+SoftwareSerial NodeMCU(D2, D3);
 
-void setup() {
- Serial.begin(115200);
- NodeMCU.begin(4800);
- pinMode(D2,INPUT);
- pinMode(D3,OUTPUT);
- 
- Serial.println();
- Serial.print("Configuring access point...");
- delay(1000);
- WiFi.begin(ssid, password);
- while (WiFi.status() != WL_CONNECTED) {
-   delay(500);
-   Serial.print(".");
- }
- Serial.println("");
- Serial.println("WiFi connected");
- Serial.println("IP address: ");
- Serial.println(WiFi.localIP());
- server.begin();
- 
+void setup()
+{
+   Serial.begin(115200);
+   NodeMCU.begin(115200);
+   pinMode(D2, INPUT);
+   pinMode(D3, OUTPUT);
+
+   Serial.println();
+   WiFiManager wifiManager;
+   Serial.println("Conecting.....");
+   wifiManager.autoConnect("Smart Shelf", "password");
+   Serial.println("connected");
+   server.begin();
 }
-void loop() {
- WiFiClient client = server.available();
- if(client) {
-  while(client.connected()) {
-    while(client.available() > 0 || NodeMCU.available() > 0) {
-      if(client.available() > 0){
-        char c = client.read();
-        Serial.write(c);
-      } else {
-        char val = NodeMCU.read();
-        if(NodeMCU.read() == '\n'){
-          Serial.println(val);
-          delay(1000);
-          client.write(val);
-        }
+void loop()
+{
+   WiFiClient client = server.available();
+   if(client)
+   {
+      while(client.connected())
+      {
+         while(NodeMCU.available() > 0)
+         {
+            float val = NodeMCU.read();
+            Serial.println(val);
+            client.println(val);
+         }
+         delay(10);
       }
-    }
-    delay(10);
-  }
-  client.stop();
-  Serial.println("Client disconnected");
- }
-} 
+      client.stop();
+      Serial.println("Client disconnected");
+   }
+}
--- senior_uno/senior_uno.ino
+++ senior_uno/senior_uno.ino
@@ -1,14 +1,72 @@
+#include <HX711_ADC.h>
+#if defined(ESP8266) || defined(ESP32) || defined(AVR)
+#include <EEPROM.h>
+#endif
 #include <SoftwareSerial.h>
-SoftwareSerial ArduinoUno(3,2);
 
-void setup(){
-  
-  Serial.begin(9600);
-  ArduinoUno.begin(4800);
+SoftwareSerial ArduinoUno(3, 2);
 
+// pins:
+const int HX711_dout = 4; // mcu > HX711 dout pin
+const int HX711_sck = 5; // mcu > HX711 sck pin
+
+// HX711 constructor:
+HX711_ADC LoadCell(HX711_dout, HX711_sck);
+
+const int calVal_eepromAdress = 0;
+unsigned long t = 0;
+
+void setup()
+{
+   Serial.begin(115200);
+   ArduinoUno.begin(115200);
+   Serial.println();
+   Serial.println("Starting...");
+
+   LoadCell.begin();
+   float calibrationValue;
+   calibrationValue = 696.0;
+#if defined(ESP8266) || defined(ESP32)
+   EEPROM.begin(512); // uncomment this if you use ESP8266/ESP32 and want to fetch the calibration value from eeprom
+#endif
+   EEPROM.get(calVal_eepromAdress, calibrationValue); // uncomment this if you want to fetch the calibration value from eeprom
+
+   unsigned long stabilizingtime = 2000; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
+   boolean _tare = true; // set this to false if you don't want tare to be performed in the next step
+   LoadCell.start(stabilizingtime, _tare);
+   if(LoadCell.getTareTimeoutFlag())
+   {
+      Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
+      while(1)
+         ;
+   }
+   else
+   {
+      LoadCell.setCalFactor(calibrationValue); // set calibration value (float)
+      Serial.println("Startup is complete");
+   }
 }
 
-void loop(){
-  ArduinoUno.print('1');
-  ArduinoUno.println('\n');
+void loop()
+{
+   static boolean newDataReady = 0;
+   const int serialPrintInterval = 1000;
+
+   // check for new data/start next conversion:
+   if(LoadCell.update())
+      newDataReady = true;
+
+   // get smoothed value from the dataset:
+   if(newDataReady)
+   {
+      if(millis() > t + serialPrintInterval)
+      {
+         float i = LoadCell.getData();
+         Serial.print("Load_cell output val: ");
+         Serial.println(i);
+         ArduinoUno.write(i);
+         newDataReady = 0;
+         t = millis();
+      }
+   }
 }
