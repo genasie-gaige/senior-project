27ad4ca863b144db34fbf039ace5c8e1ad716db1
genasie-gaige <genasie.gaige@gmail.com>
2022-04-20 00:13:04 -0400

    small fix

--- server/index.js
+++ server/index.js
@@ -13,7 +13,6 @@ const db = mysql.createConnection({
     database: 'userAuth'
 })
 
-//user database
 app.post('/create', (req, res) => {
     const userId = req.body.userId
     const password = req.body.password
@@ -33,35 +32,6 @@ app.post('/create', (req, res) => {
 
 app.get('/users', (req, res) => {
     db.query('SELECT * FROM users', (err, result) => {
-        if (err) {
-            console.log(err)
-        } else {
-            res.send(result)
-        }
-    })
-})
-
-//meds database
-app.post('/addMed', (req, res) => {
-    const idMeds = req.body.idMeds
-    const user = req.body.user
-    const med = req.body.med
-    const curWeight = req.body.curWeight
-    const startWeight = startWeight
-
-    db.query('INSERT INTO meds (idMeds, user, med, curWeight, startWeight) VALUES (?,?,?,?,?)',
-        [idMeds, user, med, curWeight, startWeight],
-        (err, result) => {
-            if (err) {
-                console.log(err)
-            } else {
-                res.send('complete')
-            }
-        })
-})
-
-app.get('/meds', (req, res) => {
-    db.query('SELECT * FROM meds', (err, result) => {
         if (err) {
             console.log(err)
         } else {
